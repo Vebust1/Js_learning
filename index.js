e74f2e5e1a059c6da90f16cd5e09300c6e188d43
Vebust <Vebust1@gmail.com>
2023-02-15 00:22:00 +0700

    my lesson

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+console.log('I like pizza')
+
+const studentName = 'Ivan'
+
+const TruthLearning = true
+
+const age = 21
+
+console.log(studentName, 'is', age, 'years old')
+console.log(`${studentName} is ${age} years old`)
+
+let username
+
+document.getElementById('myButton').onclick = function() {
+
+    username = document.getElementById('myText').value;
+    console.log(username)
+    document.getElementById('myLabel').innerHTML =`Hello ${username}` ;
+}
+
+a = window.prompt('Введите число a')
+b = window.prompt('Введите число b')
+
+const c = Math.sqrt(a * a + b * b)
+
+console.log(`Ваша гипотенуза = ${c}`)
+*/
+let x = Math.floor(Math.random() * 6) + 1
+
+console.log(`Ваше число = ${x}`)
